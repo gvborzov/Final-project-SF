b2217f1d387a6cd16c6edd36e9717629d4c93adb
gvbor <gvborzov@gmail.com>
2024-05-28 16:33:10 +0500

    first commit

---
+++ database/offers_202401191544.sql
@@ -0,0 +1,3 @@
+INSERT INTO laravel.offers (name,theme,cost,url,owner,subscribers,is_active,created_at,updated_at) VALUES
+	 ('Метросеть','Интернет провайдер',30,'https://www.metro-set.ru/',19,2,1,'2024-05-27 19:11:36','2024-05-27 19:48:22'),
+	 ('Данцер','Интернет провайдер',20,'https://dantser.ru/',20,1,1,'2024-05-27 19:13:13','2024-05-27 19:15:18');
